831a8f158f9c654d3be639458d50aebed0ae9d6f
rehanshaikh25 <shakilrehan23@gmail.com>
2024-08-06 19:27:47 +0530

    initial commit

---
+++ .evershop/build/frontStore/productView/server/entry.jsx
@@ -0,0 +1,118 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import Area from '@evershop/evershop/src/components/common/Area';
+Area.defaultProps.components = {
+  'icon-wrapper': {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL2FsbC9TZWFyY2hCb3guanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL2FsbC9TZWFyY2hCb3guanN4',
+      sortOrder: 5,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/all/SearchBox.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2hlY2tvdXQvcGFnZXMvZnJvbnRTdG9yZS9hbGwvTWluaUNhcnQuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2hlY2tvdXQvcGFnZXMvZnJvbnRTdG9yZS9hbGwvTWluaUNhcnQuanN4',
+      sortOrder: 10,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/checkout/pages/frontStore/all/MiniCart.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01vYmlsZU1lbnUuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01vYmlsZU1lbnUuanN4',
+      sortOrder: 50,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/MobileMenu.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hbGwvVXNlckljb24uanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hbGwvVXNlckljb24uanN4',
+      sortOrder: 30,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/customer/pages/frontStore/all/UserIcon.jsx')
+    }
+  },
+  productViewGeneralInfo: {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L0F0dHJpYnV0ZXMuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L0F0dHJpYnV0ZXMuanN4',
+      sortOrder: 25,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/productView/Attributes.jsx')
+    }
+  },
+  productPageMiddleRight: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L0Rlc2NyaXB0aW9uLmpzeA==': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L0Rlc2NyaXB0aW9uLmpzeA==',
+      sortOrder: 50,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/productView/Description.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L0Zvcm0uanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L0Zvcm0uanN4',
+      sortOrder: 20,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/productView/Form.jsx')
+    },
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L0dlbmVyYWxJbmZvLmpzeA==': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L0dlbmVyYWxJbmZvLmpzeA==',
+      sortOrder: 10,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/productView/GeneralInfo.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L09wdGlvbnMuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L09wdGlvbnMuanN4',
+      sortOrder: 30,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/productView/Options.jsx')
+    },
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L1ZhcmlhbnRzLmpzeA==': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L1ZhcmlhbnRzLmpzeA==',
+      sortOrder: 40,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/productView/Variants.jsx')
+    }
+  },
+  productPageMiddleLeft: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L0ltYWdlcy5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L0ltYWdlcy5qc3g=',
+      sortOrder: 10,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/productView/Images.jsx')
+    }
+  },
+  content: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L0xheW91dC5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL3Byb2R1Y3RWaWV3L0xheW91dC5qc3g=',
+      sortOrder: 10,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/productView/Layout.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0JyZWFkY3J1bWIuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0JyZWFkY3J1bWIuanN4',
+      sortOrder: 0,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Breadcrumb.jsx')
+    }
+  },
+  footer: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0Zvb3Rlci5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0Zvb3Rlci5qc3g=',
+      sortOrder: 10,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Footer.jsx')
+    }
+  },
+  head: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0hlYWRUYWdzLmpzeA==': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0hlYWRUYWdzLmpzeA==',
+      sortOrder: 5,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/HeadTags.jsx')
+    }
+  },
+  body: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xheW91dC5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xheW91dC5qc3g=',
+      sortOrder: 1,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Layout.jsx')
+    },
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL05vdGlmaWNhdGlvbi5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL05vdGlmaWNhdGlvbi5qc3g=',
+      sortOrder: 10,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Notification.jsx')
+    }
+  },
+  header: {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xvZ28uanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xvZ28uanN4',
+      sortOrder: 1,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Logo.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01lbnUuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01lbnUuanN4',
+      sortOrder: 5,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Menu.jsx')
+    }
+  }
+} 
---
+++ .evershop/build/frontStore/register/client/entry.jsx
@@ -0,0 +1,84 @@
+
+      import React from 'react';
+      import ReactDOM from 'react-dom';
+      import Area from '@evershop/evershop/src/components/common/Area';
+      import Hydrate from '@evershop/evershop/src/components/common/react/client/HydrateFrontStore';
+      
+Area.defaultProps.components = {
+  'icon-wrapper': {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL2FsbC9TZWFyY2hCb3guanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL2FsbC9TZWFyY2hCb3guanN4',
+      sortOrder: 5,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/all/SearchBox.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2hlY2tvdXQvcGFnZXMvZnJvbnRTdG9yZS9hbGwvTWluaUNhcnQuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2hlY2tvdXQvcGFnZXMvZnJvbnRTdG9yZS9hbGwvTWluaUNhcnQuanN4',
+      sortOrder: 10,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/checkout/pages/frontStore/all/MiniCart.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01vYmlsZU1lbnUuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01vYmlsZU1lbnUuanN4',
+      sortOrder: 50,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/MobileMenu.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hbGwvVXNlckljb24uanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hbGwvVXNlckljb24uanN4',
+      sortOrder: 30,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/customer/pages/frontStore/all/UserIcon.jsx')
+    }
+  },
+  content: {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0JyZWFkY3J1bWIuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0JyZWFkY3J1bWIuanN4',
+      sortOrder: 0,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Breadcrumb.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9yZWdpc3Rlci9SZWdpc3RlckZvcm0uanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9yZWdpc3Rlci9SZWdpc3RlckZvcm0uanN4',
+      sortOrder: 10,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/customer/pages/frontStore/register/RegisterForm.jsx')
+    }
+  },
+  footer: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0Zvb3Rlci5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0Zvb3Rlci5qc3g=',
+      sortOrder: 10,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Footer.jsx')
+    }
+  },
+  head: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0hlYWRUYWdzLmpzeA==': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0hlYWRUYWdzLmpzeA==',
+      sortOrder: 5,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/HeadTags.jsx')
+    }
+  },
+  body: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xheW91dC5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xheW91dC5qc3g=',
+      sortOrder: 1,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Layout.jsx')
+    },
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL05vdGlmaWNhdGlvbi5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL05vdGlmaWNhdGlvbi5qc3g=',
+      sortOrder: 10,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Notification.jsx')
+    }
+  },
+  header: {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xvZ28uanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xvZ28uanN4',
+      sortOrder: 1,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Logo.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01lbnUuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01lbnUuanN4',
+      sortOrder: 5,
+      component: require('/home/niko/crystal-pures/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Menu.jsx')
+    }
+  }
+} 
+ReactDOM.hydrate(
+        <Hydrate/>,
+        document.getElementById('app')
+      );
